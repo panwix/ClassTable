47abf90ca06c58687a1290c80d3629f96d9c5016
Panwix <1369834653@qq.com>
2015-07-18 23:48:53 +0800

    2015/07/18

--- app/src/main/java/com/panwix/classtable/MainActivity.java
+++ app/src/main/java/com/panwix/classtable/MainActivity.java
@@ -2,13 +2,141 @@ package com.panwix.classtable;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	// 周一的课；
+	public TextView c11;
+	public TextView c12;
+	public TextView c13;
+	public TextView c14;
+	public TextView c15;
+	public TextView c16;
+	public TextView c17;
+	public TextView c18;
+	public TextView c19;
+	public TextView c110;
+	public TextView c111;
+
+	// 周二的课
+	public TextView c21;
+	public TextView c22;
+	public TextView c23;
+	public TextView c24;
+	public TextView c25;
+	public TextView c26;
+	public TextView c27;
+	public TextView c28;
+	public TextView c29;
+	public TextView c210;
+	public TextView c211;
+
+	// 周三的课
+	public TextView c31;
+	public TextView c32;
+	public TextView c33;
+	public TextView c34;
+	public TextView c35;
+	public TextView c36;
+	public TextView c37;
+	public TextView c38;
+	public TextView c39;
+	public TextView c310;
+	public TextView c311;
+
+	// 周四的课
+	public TextView c41;
+	public TextView c42;
+	public TextView c43;
+	public TextView c44;
+	public TextView c45;
+	public TextView c46;
+	public TextView c47;
+	public TextView c48;
+	public TextView c49;
+	public TextView c410;
+	public TextView c411;
+
+	// 周五的课
+	public TextView c51;
+	public TextView c52;
+	public TextView c53;
+	public TextView c54;
+	public TextView c55;
+	public TextView c56;
+	public TextView c57;
+	public TextView c58;
+	public TextView c59;
+	public TextView c510;
+	public TextView c511;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_layout);
+
+		// 绑定周一的课的TextView
+		c11 = (TextView)findViewById(R.id.c11);
+		c12 = (TextView)findViewById(R.id.c12);
+		c13 = (TextView)findViewById(R.id.c13);
+		c14 = (TextView)findViewById(R.id.c14);
+		c15 = (TextView)findViewById(R.id.c15);
+		c16 = (TextView)findViewById(R.id.c16);
+		c17 = (TextView)findViewById(R.id.c17);
+		c18 = (TextView)findViewById(R.id.c18);
+		c19 = (TextView)findViewById(R.id.c19);
+		c110 = (TextView)findViewById(R.id.c110);
+		c111 = (TextView)findViewById(R.id.c111);
+		// 绑定周二的课的TextView
+		c21 = (TextView)findViewById(R.id.c21);
+		c22 = (TextView)findViewById(R.id.c22);
+		c23 = (TextView)findViewById(R.id.c23);
+		c24 = (TextView)findViewById(R.id.c24);
+		c25 = (TextView)findViewById(R.id.c25);
+		c26 = (TextView)findViewById(R.id.c26);
+		c27 = (TextView)findViewById(R.id.c27);
+		c28 = (TextView)findViewById(R.id.c28);
+		c29 = (TextView)findViewById(R.id.c29);
+		c210 = (TextView)findViewById(R.id.c210);
+		c211 = (TextView)findViewById(R.id.c211);
+		// 绑定周三的课的TextView
+		c31 = (TextView)findViewById(R.id.c31);
+		c32 = (TextView)findViewById(R.id.c32);
+		c33 = (TextView)findViewById(R.id.c33);
+		c34 = (TextView)findViewById(R.id.c34);
+		c35 = (TextView)findViewById(R.id.c35);
+		c36 = (TextView)findViewById(R.id.c36);
+		c37 = (TextView)findViewById(R.id.c37);
+		c38 = (TextView)findViewById(R.id.c38);
+		c39 = (TextView)findViewById(R.id.c39);
+		c310 = (TextView)findViewById(R.id.c310);
+		c311 = (TextView)findViewById(R.id.c311);
+		// 绑定周四的课的TextView
+		c41 = (TextView)findViewById(R.id.c41);
+		c42 = (TextView)findViewById(R.id.c42);
+		c43 = (TextView)findViewById(R.id.c43);
+		c44 = (TextView)findViewById(R.id.c44);
+		c45 = (TextView)findViewById(R.id.c45);
+		c46 = (TextView)findViewById(R.id.c46);
+		c47 = (TextView)findViewById(R.id.c47);
+		c48 = (TextView)findViewById(R.id.c48);
+		c49 = (TextView)findViewById(R.id.c49);
+		c410 = (TextView)findViewById(R.id.c410);
+		c411 = (TextView)findViewById(R.id.c411);
+		// 绑定周五的课的TextView
+		c51 = (TextView)findViewById(R.id.c51);
+		c52 = (TextView)findViewById(R.id.c52);
+		c53 = (TextView)findViewById(R.id.c53);
+		c54 = (TextView)findViewById(R.id.c54);
+		c55 = (TextView)findViewById(R.id.c55);
+		c56 = (TextView)findViewById(R.id.c56);
+		c57 = (TextView)findViewById(R.id.c57);
+		c58 = (TextView)findViewById(R.id.c58);
+		c59 = (TextView)findViewById(R.id.c59);
+		c510 = (TextView)findViewById(R.id.c510);
+		c511 = (TextView)findViewById(R.id.c511);
+
 	}
 
 
